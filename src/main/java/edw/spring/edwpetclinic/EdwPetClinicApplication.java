3f4eb8631893e2390869d8d813ce8c661303e594
Eduard Cojocaru <eduard.cojocaru@orange.com>
2020-04-17 12:42:32 +0300

    initial project add

---
+++ src/main/java/edw/spring/edwpetclinic/EdwPetClinicApplication.java
@@ -0,0 +1,13 @@
+package edw.spring.edwpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdwPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdwPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edw/spring/edwpetclinic/EdwPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package edw.spring.edwpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdwPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
